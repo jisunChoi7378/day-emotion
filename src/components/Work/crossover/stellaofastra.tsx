3e543ab223c6da6b09f463c632a45330adb8d25e
123456466 <sunji7378@gmail.com>
2025-12-25 19:45:41 +0900

    feat: 작품 추가

---
+++ src/components/Work/crossover/stellaofastra.tsx
@@ -0,0 +1,24 @@
+import Image from "next/image";
+
+const SR_D_BS2 = () => {
+  return (
+    <article className="work-content work-novel">
+      <header className="work-header">
+        <h2 className="work-character center">
+          검은방, 회색도시, 베리드 스타즈
+        </h2>
+        <div className="work-author">written by. 이즈라크</div>
+      </header>
+      <div className="flex justify-center">
+        <Image
+          src="/paintingWork/crossover/크리스마스합작_그림_이즈라크_양수연권혜연민주영_비극을 든 산타클로스.png"
+          alt="이즈라크님의 아름다운 크오작품"
+          width={400}
+          height={400}
+        />
+      </div>
+    </article>
+  );
+};
+
+export default SR_D_BS2;
--- src/components/common/Modal.tsx
+++ src/components/common/Modal.tsx
@@ -1,4 +1,5 @@
-import { useOpenProfileModalStore } from "@/store/modal";
+import {useOpenProfileModalStore} from "@/store/modal";
+import {forwardRef} from "react";
 
 interface ProfileData {
   className?: string;
@@ -6,29 +7,34 @@ interface ProfileData {
   children?: React.ReactNode;
 }
 
-const Modal = ({ className, backgroundClick, children }: ProfileData) => {
-  const { isOpen, close, setWork, modalClass } = useOpenProfileModalStore();
+const Modal = forwardRef<HTMLDivElement, ProfileData>(
+  ({className, backgroundClick, children}, ref) => {
+    const {isOpen, close, setWork, modalClass} = useOpenProfileModalStore();
 
-  const handleClose = () => {
-    close();
-  };
+    const handleClose = () => {
+      close();
+    };
 
-  return (
-    <div
-      className={`fixed z-50 inset-0 flex items-center justify-center bg-[#00000051] ${isOpen ? "" : "hidden"}`}
-      onClick={backgroundClick ? backgroundClick : handleClose}
-    >
+    return (
       <div
-        className={`${modalClass && modalClass.includes("no-modal-bg") ? "" : "bg-[#979797]"} ${modalClass ?? className ?? ""} cursor-default overflow-auto h-full md:max-h-[90vh] w-full md:max-w-[70%]`}
-        onClick={(e) => e.stopPropagation()}
+        className={`fixed z-50 inset-0 flex items-center justify-center bg-[#00000051] ${isOpen ? "" : "hidden"}`}
+        onClick={backgroundClick ? backgroundClick : handleClose}
       >
-        <div className="md:hidden" onClick={handleClose}>
-          X
+        <div
+          ref={ref} // ✅ ref 추가
+          className={`${modalClass && modalClass.includes("no-modal-bg") ? "" : "bg-[#979797]"} ${modalClass ?? className ?? ""} cursor-default overflow-auto h-full md:max-h-[90vh] w-full md:max-w-[70%]`}
+          onClick={(e) => e.stopPropagation()}
+        >
+          <div className="md:hidden" onClick={handleClose}>
+            X
+          </div>
+          {setWork ?? children ?? null}
         </div>
-        {setWork ?? children ?? null}
       </div>
-    </div>
-  );
-};
+    );
+  }
+);
+
+Modal.displayName = "Modal";
 
 export default Modal;
